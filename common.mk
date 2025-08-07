0e0e18b8bb84a7b9dfc57c51a07aacd17457b6d5
Zachary Yedidia <zyedidia@gmail.com>
2025-08-06 17:09:08 -0700

    Add lfi include path

--- common.mk
+++ common.mk
@@ -1,5 +1,5 @@
 LFI_CC ?= x86_64-lfi-linux-musl-clang
 LFI_BIND ?= lfi-bind
 
-RLBOX_INCLUDE = -I$(ROOT)/deps/rlbox/code/include -I$(ROOT)/deps/rlbox_lfi_sandbox/include
+RLBOX_INCLUDE = -I$(ROOT)/deps/rlbox/code/include -I$(ROOT)/deps/rlbox_lfi_sandbox/include -I$(ROOT)/deps/install/include
 LIBLFI = $(ROOT)/deps/install/lib/liblfi.a
